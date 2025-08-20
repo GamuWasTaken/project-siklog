d781cccb86fab78986f511307b0c8d32b819d697
GamuWasTaken <victordiezruiz@proton.me>
2025-08-20 23:12:23 +0200

    Updated gh-pages

--- cache-worker.js
+++ cache-worker.js
@@ -11,3 +11,23 @@ self.addEventListener('install', event => {
     ])
   )
 })
+
+const cacheFirst = async (request, event) => {
+  const responseFromCache = await caches.match(request)
+
+  if (responseFromCache) return responseFromCache
+
+  const responseFromNetwork = await fetch(request)
+  event.waitUntil(putInCache(request, responseFromNetwork.clone()))
+
+  return responseFromNetwork
+}
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1')
+  await cache.put(request, response)
+}
+
+self.addEventListener('fetch', event => {
+  event.respondWith(cacheFirst(event.request, event))
+})
