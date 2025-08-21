f7413032636d755740b3c8698fbbc109a50f8b7e
GamuWasTaken <victordiezruiz@proton.me>
2025-08-21 19:55:51 +0200

    Updated gh-pages

---
+++ _app/immutable/chunks/1_m_8f3n.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_gh9iny?.base??"/project-siklog",e=globalThis.__sveltekit_gh9iny?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/C0MNJ-sL.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1nzzj82?.base??"/project-siklog",e=globalThis.__sveltekit_1nzzj82?.assets??s;export{e as a,s as b};
